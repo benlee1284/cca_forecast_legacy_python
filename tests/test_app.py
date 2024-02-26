d5d836d7c431772cbbaca27ffd3c7c99e5f4c163
Sean <sean@kayenta.io>
2024-02-26 16:42:07 +0000

    parametrize test_get_average_value

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,18 @@
 from src.app import get_average_value
+from typing import Union
 import pytest
 
 
-def test_get_average_value():
-    assert 1 == get_average_value([1])
+@pytest.mark.parametrize(
+    "values, decimal_places, expected_result",
+    [
+        ([1, 2, 3, 4, 5], None, 3),
+        ([1.1, 2.2, 3.3, 4.4, 5.5], 2, 3.3),
+    ]
+)
+def test_get_average_value(
+        values: list[Union[float, int]],
+        decimal_places: Union[int, None],
+        expected_result: Union[int, float],
+) -> None:
+    assert expected_result == get_average_value(values, decimal_places)
